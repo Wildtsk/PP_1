d75f7e8a6e4510d736e6edf7ee3439d00ce4d865
Wildtsk <wildtsk@gmail.com>
2022-07-17 20:31:28 +0300

    Wrote an algorithm

--- PJ_1/utils.py
+++ PJ_1/utils.py
@@ -1,1 +1,18 @@
-hello
+def generates_numbers():
+    """Геренрирует 3 рандомных числа"""
+    pass
+
+
+def get_number_user(number: str) -> list:
+    """Получает 3х значное число от пользователя"""
+    pass
+
+
+def compares_received_numbers():
+    """Сравнивает полученные числа с загаданным"""
+    pass
+
+
+def result_comparison():
+    """Выводит результат сравнения"""
+    pass
